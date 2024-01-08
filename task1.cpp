bcd70ae3a85ec782c71198b474187bd4ab6a6cd1
MuhammadHassan05 <156007695+MuhammadHassan05@users.noreply.github.com>
2024-01-09 00:58:38 +0500

    Add files via upload
    C++ program to reverse the order of the DIGITS entered by the user.

---
+++ task1.cpp
@@ -0,0 +1,10 @@
+////new material
+// //#include<iostream>
+//using namespace std;
+//int main() {
+//	int val=0, sum=0;
+//	while (cin >> val)
+//		sum = sum + val;
+//	cout << "sum=" << sum << endl;
+//	return 0;
+//}
