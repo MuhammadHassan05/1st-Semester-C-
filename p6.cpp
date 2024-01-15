bc6e1269113ad5e385b5fac538d439cf25ef2877
MuhammadHassan05 <156007695+MuhammadHassan05@users.noreply.github.com>
2024-01-16 00:22:20 +0500

    calculator using C++

---
+++ p6.cpp
@@ -0,0 +1,34 @@
+//#include<iostream>
+//using namespace std;
+//int main() {
+//	int num1;
+//	int num2;
+//	int operater;
+//	cout << "enter num1"<<endl;
+//	cout << "enter num2"<<endl;
+//	cin >> num1;
+//	cin >> num2;
+//
+//	cout << "1.addition<<endl<<2.subtraction<<endl<<3.multiplication<<endl<<4.division<<endl<<5.mod"<<endl;
+//	cin >> operater;
+//	switch (operater){
+//	case 1:
+//		cout << "add" << num1 + num2;
+//		break;
+//	case 2:
+//		cout << "subtract" << num1 - num2;
+//		break;
+//	case 3:
+//		cout << "multiply" << num1 * num2;
+//		break;
+//	case 4:
+//		cout << "division" << num1 / num2;
+//		break;
+//	case 5:
+//		cout << "mod" << num1 % num2;
+//		break;
+//	default:
+//		cout << "invalid operator selected";
+//	}
+//	return 0;
+//}
